04e94202d08bf9e87c1a0aa18310ed9490d6aebd
eva-yanrong-chen <eva.yanrong.chen@gmail.com>
2020-09-22 20:00:09 -0400

    Update to work with subject & condition data

--- script.py
+++ script.py
@@ -1,12 +1,84 @@
-from data_processing import *
 from model_training import *
+from data_processing import *
+import pandas as pd
+from numpy.random import seed
+import tensorflow as tf
 
-# segment_data()
-manual_label_data(1)
-# data = label_data(data)
-# data = cut_data(data)
-# ###################### Training Neural Network ##################
-# window_sizes = np.arange(20, 221, 20)
+# Seed Random Number Generators for Reproducibility
+seed(1)
+tf.random.set_seed(seed=5)
 
-# train_cnn(data, window_sizes, ['adam'])
-# plot_err(window_sizes * 5)
+subjects = np.arange(1, 11)
+
+# # CNN Model
+# hyperparam_space = {
+#     'subject': subjects,
+#     'fold': ['pair'],
+#     'window_size': [100],
+#     'model': 'cnn',
+#     'cnn': {
+#       'kernel_size': [10],
+#       'activation': ['relu']
+#     },
+#     'dense': {
+#         'activation': ['tanh']
+#     },
+#     'optimizer': {
+#         'loss': ['mean_absolute_error'],
+#         'optimizer': ['adam']
+#     },
+#     'training': {
+#         'epochs': [10],
+#         'batch_size': [128]
+#     }
+# }
+
+# # MLP Model
+# hyperparam_space = {
+#     'subject': subjects,
+#     'fold': ['pair'],
+#     'window_size': [20],
+#     'model': 'mlp',
+#     'dense': {
+#         'num_layers': [1],
+#         'num_nodes': [5],
+#         'activation': ['tanh']
+#     },
+#     'optimizer': {
+#         'loss': ['mean_absolute_error'],
+#         'optimizer': ['adam']
+#     },
+#     'training': {
+#         'epochs': [10],
+#         'batch_size': [128]
+#     }
+# }
+
+## LSTM Model
+# hyperparam_space = {
+#     'subject': subjects,
+#     'fold': ['pair'],
+#     'window_size': [40],
+#     'model': 'lstm',
+#     'lstm': {
+#       'units': [20],
+#       'activation': ['tanh']
+#     },
+#     'dense': {
+#         'activation': ['relu']
+#     },
+#     'optimizer': {
+#         'loss': ['mean_absolute_error'],
+#         'optimizer': ['adam']
+#     },
+#     'training': {
+#         'epochs': [2],
+#         'batch_size': [128]
+#     }
+# }
+
+hyperparameter_configs = get_model_configs_subject(hyperparam_space)
+
+data = import_subject_data(subjects)
+
+trial_results, average_results = train_models_subject(hyperparam_space['model'], hyperparameter_configs, data)
