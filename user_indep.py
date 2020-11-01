d853d7dad654d51d535ec33722aa69fc10055691
Srijan Duggal <srijanduggal17@gmail.com>
2020-11-01 15:25:06 -0500

    setting up user independent branch

---
+++ user_indep.py
@@ -0,0 +1,15 @@
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+from data_processing import *
+from model_training import *
+from data_cleaning import *
+
+# Load eval data for all subjects
+
+# List hyperparameters to sweep
+
+# Train models and evaluate
+
+# Store results in file
